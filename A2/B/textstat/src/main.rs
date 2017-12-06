03da1b4fd1d8013fce36637f44cdf1c5e6fe19e1
werew <luigi.coniglio@yahoo.com>
2017-12-06 17:14:54 -0500

    Add base code

--- A2/B/textstat/src/main.rs
+++ A2/B/textstat/src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::process;
+use std::fs::File;
+use std::error::Error;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("usage: {} <filename>",args[0]);
+        process::exit(1);
+    }
+
+    let filename = &args[1];
+    let f = match File::open(&filename) {
+        Ok(f)  => f,
+        Err(e) => {
+            eprintln!("Cannot open file: {}", e.description());
+            process::exit(1);
+        }
+    };
+
 }
