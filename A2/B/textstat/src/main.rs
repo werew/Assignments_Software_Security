7ebf107dc9fa11401780c4b41814779ae527ded6
werew <luigi.coniglio@yahoo.com>
2017-12-07 10:32:23 -0500

    Handle IO errors

--- A2/B/textstat/src/main.rs
+++ A2/B/textstat/src/main.rs
@@ -4,15 +4,44 @@ use std::fs::File;
 use std::error::Error;
 use std::io::Read;
 use std::io::BufReader;
+use std::io::Bytes;
+use std::io::Result;
+
+
+fn read_word(it: &mut Bytes<BufReader<File>>) -> Result<String> {
+    let mut w = String::with_capacity(100);
+    loop {
+        // Read next byte
+        let r = match it.next() {
+            Some(r) => r,
+            None =>  break, // EOF
+        };
+
+        // Treat IO errors
+        if r.is_err() { 
+            return Err(r.unwrap_err()); 
+        }
+
+        w.push(r.unwrap() as char);
+    }
+
+    println!("{}",w);
+    Ok(w)
+}
+
 
 fn get_stats(buf: BufReader<File>){
 
+    let mut it = buf.bytes();
+    read_word(&mut it);
+    /*
     for r in buf.bytes() {
         let b = r.unwrap() as char;
         //println!("Byte: {}",std::str::from_utf8(&[b]).unwrap());
         println!("Byte: {}",b.is_uppercase());
 
     }
+    */
 
 }
 
