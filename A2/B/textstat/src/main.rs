71900e9f3abbbfea61edf38ae59f2b7851447803
werew <luigi.coniglio@yahoo.com>
2017-12-06 19:18:06 -0500

    Read byte by byte

--- A2/B/textstat/src/main.rs
+++ A2/B/textstat/src/main.rs
@@ -2,11 +2,24 @@ use std::env;
 use std::process;
 use std::fs::File;
 use std::error::Error;
+use std::io::Read;
+use std::io::BufReader;
 
+fn get_stats(buf: BufReader<File>){
+
+    for r in buf.bytes() {
+        let b = r.unwrap() as char;
+        //println!("Byte: {}",std::str::from_utf8(&[b]).unwrap());
+        println!("Byte: {}",b.is_uppercase());
+
+    }
+
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
+    let args: Vec<String> = env::args().collect();
+   
     if args.len() < 2 {
         eprintln!("usage: {} <filename>",args[0]);
         process::exit(1);
@@ -21,4 +34,7 @@ fn main() {
         }
     };
 
+   
+    let buf = BufReader::new(f);
+    get_stats(buf);
 }
