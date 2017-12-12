59b70fdefe42715780e3d719f91bed648d865a78
werew <luigi.coniglio@yahoo.com>
2017-12-12 13:00:33 -0500

    Adding a bunch of tests

--- A2/rustsint/src/sortedcontainer.rs
+++ A2/rustsint/src/sortedcontainer.rs
@@ -11,9 +11,6 @@ struct Node<T> {
 }
 
 
-
-
-
 pub struct SortedContainer<T> {
     root: Link<T>,
 }
@@ -74,7 +71,6 @@ impl<T: Debug + Display + PartialOrd> SortedContainer<T> {
         if target.is_none() { return; } // None not found
 
         let mut target_content = *target.take().unwrap();       // Note, dereference the box!
-       
 
         if target_content.left.is_some() && target_content.right.is_some() {
 
@@ -202,5 +198,514 @@ impl<T: Debug + Display + PartialOrd> SortedContainer<T> {
         self.root = root;
         */
 
+}
+
+
+
+
+
+// A basic test testing mostly a normal usage of the public API 
+#[test]
+fn test_base_api(){
+
+        let mut sc : SortedContainer<u32> = SortedContainer::new();
+
+        // A new container should be empty
+        assert_eq!(sc.root.is_none(), true, "Root not empty");
+
+        sc.insert(5); sc.insert(2); sc.insert(7); 
+        sc.insert(4); sc.insert(1); sc.insert(3);
+        sc.insert(6); sc.insert(9); sc.insert(8);
+
+
+        // After an insertion the container should not be empty
+        assert_eq!(sc.root.is_none(), false, "Root is empty");
+
+
+        // The container should contain all the values inserted 
+        assert!(sc.contains(1), "Does not contain value 1");
+        assert!(sc.contains(2), "Does not contain value 2");
+        assert!(sc.contains(3), "Does not contain value 3");
+        assert!(sc.contains(4), "Does not contain value 4");
+        assert!(sc.contains(5), "Does not contain value 5");
+        assert!(sc.contains(6), "Does not contain value 6");
+        assert!(sc.contains(7), "Does not contain value 7");
+        assert!(sc.contains(8), "Does not contain value 8");
+        assert!(sc.contains(9), "Does not contain value 9");
+
+
+        sc.erase(5); sc.erase(3); sc.erase(8);
+
+
+        // The container should not contain the erased values
+        assert!(!sc.contains(5), "Contains value 5");
+        assert!(!sc.contains(3), "Contains value 3");
+        assert!(!sc.contains(8), "Contains value 8");
+
+
+        // ..but should still contain the value not erased
+        assert!(sc.contains(1), "Does not contain value 1");
+        assert!(sc.contains(2), "Does not contain value 2");
+        assert!(sc.contains(4), "Does not contain value 4");
+        assert!(sc.contains(6), "Does not contain value 6");
+        assert!(sc.contains(7), "Does not contain value 7");
+        assert!(sc.contains(9), "Does not contain value 9");
+
+
+        sc.erase(1); sc.erase(2); sc.erase(4);
+        sc.erase(6); sc.erase(7); sc.erase(9);
+
+
+        // After erasing all the values the container should be empty
+        assert_eq!(sc.root.is_none(), true, "Root not empty");
+
+}
+
+
+// 
+#[test]
+fn test_double_erase(){
+
+        let mut sc : SortedContainer<u32> = SortedContainer::new();
+
+        sc.insert(1); sc.insert(2);
+
+        // The container should contain all the values inserted 
+        assert!(sc.contains(1), "Does not contain value 1");
+        assert!(sc.contains(2), "Does not contain value 2");
+
+        sc.erase(1); sc.erase(1);
+
+        // The container should not contain the erased values
+        assert!(!sc.contains(1), "Contains value 1");
+
+        // ..but should still contain the value not erased
+        assert!(sc.contains(2), "Does not contain value 2");
+}
+
+
+
+
+// Test the deletion of a node with no children
+#[test]
+fn test_erase_no_children(){
+
+        let mut sc : SortedContainer<u32> = SortedContainer::new();
+        assert_eq!(sc.root.is_none(), true);
+
+        sc.insert(2); 
+        sc.insert(1);
+        sc.insert(3);
+
+        /*
+         *  1. Test tree's initial configuration:
+         *
+         *          2
+         *         / \
+         *        1   3
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 2, "Data not equivalent"); 
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 1, "Data not equivalent");
+            assert_eq!(left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left.right.is_none(), true, "Branch should be empty");
+
+            let right = root.right.as_ref().unwrap();
+            assert_eq!(right.data, 3, "Data not equivalent");
+            assert_eq!(right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(right.right.is_none(), true, "Branch should be empty");
+        }
+
+        sc.erase(3);
+
+        /*
+         *  2. Test tree's configuration after deleting 3:
+         *
+         *          2
+         *         / 
+         *        1  
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 2, "Data not equivalent"); 
+            assert_eq!(root.right.is_none(), true, "Branch should be empty");
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 1, "Data not equivalent");
+            assert_eq!(left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left.right.is_none(), true, "Branch should be empty");
+
+        }
+}
+
+
+#[test]
+fn test_erase_with_one_child(){
+
+        let mut sc : SortedContainer<u32> = SortedContainer::new();
+        assert_eq!(sc.root.is_none(), true);
+
+        sc.insert(3);
+        sc.insert(1); 
+        sc.insert(2);
+
+        /*
+         *  1. Test tree's initial configuration:
+         *
+         *          3
+         *         /
+         *        1  
+         *         \
+         *          2
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 3, "Data not equivalent"); 
+            assert_eq!(root.right.is_none(), true, "Branch should be empty");
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 1, "Data not equivalent");
+            assert_eq!(left.left.is_none(), true, "Branch should be empty");
+
+            let left_right = left.right.as_ref().unwrap();
+            assert_eq!(left_right.data, 2, "Data not equivalent");
+            assert_eq!(left_right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_right.right.is_none(), true, "Branch should be empty");
+        }
+
+        sc.erase(1);
+
+        /*
+         *  2. Test tree's configuration after deleting 1:
+         *
+         *          3
+         *         /
+         *        2  
+         *         
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 3, "Data not equivalent"); 
+            assert_eq!(root.right.is_none(), true, "Branch should be empty");
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 2, "Data not equivalent");
+            assert_eq!(left.left.is_none(), true, "Branch should be empty");
+            assert_eq!(left.right.is_none(), true, "Branch should be empty");
+
+        }
+}
+
+
+
+// Test the deletion of a node with two children twice (if node is root, or not)
+#[test]
+fn test_erase_with_two_childen_1(){
+
+        let mut sc : SortedContainer<u32> = SortedContainer::new();
+        assert_eq!(sc.root.is_none(), true);
+
+        sc.insert(2); sc.insert(1); sc.insert(4); 
+        sc.insert(3); sc.insert(5); 
+
+        /*
+         *  1. Test tree's initial configuration:
+         *
+         *          2
+         *         / \
+         *        1   4
+         *           / \ 
+         *          3   5
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 2, "Data not equivalent"); 
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 1, "Data not equivalent");
+            assert_eq!(left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left.right.is_none(), true, "Branch should be empty");
+
+            let right = root.right.as_ref().unwrap();
+            assert_eq!(right.data, 4, "Data not equivalent");
+
+            let right_left = right.left.as_ref().unwrap();
+            assert_eq!(right_left.data, 3, "Data not equivalent");
+            assert_eq!(right_left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(right_left.right.is_none(), true, "Branch should be empty");
+
+            let right_right = right.right.as_ref().unwrap();
+            assert_eq!(right_right.data, 5, "Data not equivalent");
+            assert_eq!(right_right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(right_right.right.is_none(), true, "Branch should be empty");
+        }
+
+
+        sc.erase(4);
+
+
+        /*
+         *  2. Test tree's configuration after deleting 4:
+         *
+         *          2
+         *         / \
+         *        1   5
+         *           /  
+         *          3   
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 2, "Data not equivalent"); 
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 1, "Data not equivalent");
+            assert_eq!(left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left.right.is_none(), true, "Branch should be empty");
+
+            let right = root.right.as_ref().unwrap();
+            assert_eq!(right.data, 5, "Data not equivalent");
+            assert_eq!(right.right.is_none(),  true, "Branch should be empty");
+
+            let right_left = right.left.as_ref().unwrap();
+            assert_eq!(right_left.data, 3, "Data not equivalent");
+            assert_eq!(right_left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(right_left.right.is_none(), true, "Branch should be empty");
+
+        }
+
+
+        sc.erase(2);
+
+        
+        /*
+         *  3. Test tree's configuration after deleting 2 (the root):
+         *
+         *          3
+         *         / \
+         *        1   5
+         *              
+         *              
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 3, "Data not equivalent"); 
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 1, "Data not equivalent");
+            assert_eq!(left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left.right.is_none(), true, "Branch should be empty");
+
+            let right = root.right.as_ref().unwrap();
+            assert_eq!(right.data, 5, "Data not equivalent");
+            assert_eq!(right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(right.right.is_none(),  true, "Branch should be empty");
+        }
+}
+
+
+
+
+
+
+// Test the deletion of a node with two children, who each have two children as well
+#[test]
+fn test_erase_with_two_childen_2(){
+
+        let mut sc : SortedContainer<u32> = SortedContainer::new();
+        assert_eq!(sc.root.is_none(), true);
+
+        sc.insert(4); sc.insert(2); sc.insert(1); 
+        sc.insert(3); sc.insert(6); sc.insert(5); 
+        sc.insert(7); 
+
+        /*
+         *  1. Test tree's initial configuration:
+         *
+         *           4
+         *          / \
+         *        2     6
+         *       / \   / \ 
+         *      1   3 5   7
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 4, "Data not equivalent"); 
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 2, "Data not equivalent");
+
+            let left_left = left.left.as_ref().unwrap();
+            assert_eq!(left_left.data, 1, "Data not equivalent");
+            assert_eq!(left_left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_left.right.is_none(), true, "Branch should be empty");
+
+            let left_right = left.right.as_ref().unwrap();
+            assert_eq!(left_right.data, 3, "Data not equivalent");
+            assert_eq!(left_right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_right.right.is_none(), true, "Branch should be empty");
+
+            let right = root.right.as_ref().unwrap();
+            assert_eq!(right.data, 6, "Data not equivalent");
+
+            let right_left = right.left.as_ref().unwrap();
+            assert_eq!(right_left.data, 5, "Data not equivalent");
+            assert_eq!(right_left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(right_left.right.is_none(), true, "Branch should be empty");
+
+            let right_right = right.right.as_ref().unwrap();
+            assert_eq!(right_right.data, 7, "Data not equivalent");
+            assert_eq!(right_right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(right_right.right.is_none(), true, "Branch should be empty");
+        }
+
+        
+        sc.erase(4);
+
+        /*
+         *  2. Test tree's configuration after deleting 4:
+         *
+         *           5
+         *          / \
+         *        2     6
+         *       / \     \ 
+         *      1   3     7
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 5, "Data not equivalent"); 
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 2, "Data not equivalent");
+
+            let left_left = left.left.as_ref().unwrap();
+            assert_eq!(left_left.data, 1, "Data not equivalent");
+            assert_eq!(left_left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_left.right.is_none(), true, "Branch should be empty");
+
+            let left_right = left.right.as_ref().unwrap();
+            assert_eq!(left_right.data, 3, "Data not equivalent");
+            assert_eq!(left_right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_right.right.is_none(), true, "Branch should be empty");
+
+            let right = root.right.as_ref().unwrap();
+            assert_eq!(right.data, 6, "Data not equivalent");
+            assert_eq!(right.left.is_none(),  true, "Branch should be empty");
+
+            let right_right = right.right.as_ref().unwrap();
+            assert_eq!(right_right.data, 7, "Data not equivalent");
+            assert_eq!(right_right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(right_right.right.is_none(), true, "Branch should be empty");
+        }
+}
+
+
+
+
+// Test the deletion of a non-root node with two children, who each have two children as well
+#[test]
+fn test_erase_with_two_childen_3(){
+
+        let mut sc : SortedContainer<u32> = SortedContainer::new();
+        assert_eq!(sc.root.is_none(), true);
+
+        sc.insert(8); sc.insert(4); sc.insert(2); 
+        sc.insert(1); sc.insert(3); sc.insert(6);
+        sc.insert(5); sc.insert(7); 
+
+        /*
+         *  1. Test tree's initial configuration:
+         *
+         *             8
+         *            /
+         *           4
+         *          / \
+         *        2     6
+         *       / \   / \ 
+         *      1   3 5   7
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 8, "Data not equivalent"); 
+            assert_eq!(root.right.is_none(),  true, "Branch should be empty");
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 4, "Data not equivalent");
+
+            let left_left = left.left.as_ref().unwrap();
+            assert_eq!(left_left.data, 2, "Data not equivalent");
+
+            let left_left_left = left_left.left.as_ref().unwrap();
+            assert_eq!(left_left_left.data, 1, "Data not equivalent");
+            assert_eq!(left_left_left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_left_left.right.is_none(),  true, "Branch should be empty");
+
+            let left_left_right = left_left.right.as_ref().unwrap();
+            assert_eq!(left_left_right.data, 3, "Data not equivalent");
+            assert_eq!(left_left_right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_left_right.right.is_none(),  true, "Branch should be empty");
+
+            let left_right = left.right.as_ref().unwrap();
+            assert_eq!(left_right.data, 6, "Data not equivalent");
+
+            let left_right_left = left_right.left.as_ref().unwrap();
+            assert_eq!(left_right_left.data, 5, "Data not equivalent");
+            assert_eq!(left_right_left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_right_left.right.is_none(),  true, "Branch should be empty");
+
+            let left_right_right = left_right.right.as_ref().unwrap();
+            assert_eq!(left_right_right.data, 7, "Data not equivalent");
+            assert_eq!(left_right_right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_right_right.right.is_none(),  true, "Branch should be empty");
+
+        }
+
+
+        sc.erase(4);
+
+        /*
+         *  2. Test tree's configuration after deleting 4:
+         *
+         *             8
+         *            /
+         *           5
+         *          / \
+         *        2     6
+         *       / \     \ 
+         *      1   3     7
+         */
+        {
+            let root = sc.root.as_ref().unwrap();
+            assert_eq!(root.data, 8, "Data not equivalent"); 
+            assert_eq!(root.right.is_none(),  true, "Branch should be empty");
+
+            let left = root.left.as_ref().unwrap();
+            assert_eq!(left.data, 5, "Data not equivalent");
+
+            let left_left = left.left.as_ref().unwrap();
+            assert_eq!(left_left.data, 2, "Data not equivalent");
+
+            let left_left_left = left_left.left.as_ref().unwrap();
+            assert_eq!(left_left_left.data, 1, "Data not equivalent");
+            assert_eq!(left_left_left.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_left_left.right.is_none(),  true, "Branch should be empty");
+
+            let left_left_right = left_left.right.as_ref().unwrap();
+            assert_eq!(left_left_right.data, 3, "Data not equivalent");
+            assert_eq!(left_left_right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_left_right.right.is_none(),  true, "Branch should be empty");
+
+            let left_right = left.right.as_ref().unwrap();
+            assert_eq!(left_right.data, 6, "Data not equivalent");
+            assert_eq!(left_right.left.is_none(),  true, "Branch should be empty");
+
+            let left_right_right = left_right.right.as_ref().unwrap();
+            assert_eq!(left_right_right.data, 7, "Data not equivalent");
+            assert_eq!(left_right_right.left.is_none(),  true, "Branch should be empty");
+            assert_eq!(left_right_right.right.is_none(),  true, "Branch should be empty");
+
+        }
 }
 
